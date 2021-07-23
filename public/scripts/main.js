a5f9bcf739207770703118c4dfc2fc677e7f4741
Rafael Machado note <rafaeldmachado2001@gmail.com>
2021-07-23 16:33:59 -0300

    Novas rotas - Modificações no script para abrir e fechar a modal - Aula 04

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,16 @@
 import { Modal } from './modal.js'
 
 const modal = Modal()
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
 
 // Pega todos os botões com a classe 'check'
 const checkButtons = document.querySelectorAll(".actions a.check");
 // Abre a modal
 checkButtons.forEach(button => {
     // Verifica os botões
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 // Pega todos os botões com a classe 'cancel'
@@ -23,7 +24,18 @@ cancelButton.addEventListener("click", ()=> {
 const deleteButton = document.querySelectorAll('.actions a.delete')
 // Abre a modal clicando no botão 'Excluir' das perguntas
 deleteButton.forEach(button => {
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    // Abrir modal
+    modal.open()
+}
--- src/route.js
+++ src/route.js
@@ -3,10 +3,12 @@ const express = require('express');
 // Define a const como uma variável de rota
 const route = express.Router();
 
-// Cria as rotas para os arquivos HTML/EJS da página
+// Cria as rotas para os arquivos da página
 route.get('/', (req, res)=> res.render('index'))
 route.get('/room', (req, res)=> res.render('room'))
 route.get('/create-room', (req, res)=> res.render('create-room'))
+// Rota para realizar as ações dentro da room
+route.post('/room/:room/:question/:action', (req, res) => res.render())
 
 // Exporta a const para poder acessar nos outros arquivos
 module.exports = route

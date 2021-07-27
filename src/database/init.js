7530eca947fd53053b2b7bfad771f47098a2d174
Rafael Machado note <rafaeldmachado2001@gmail.com>
2021-07-27 09:41:46 -0300

    Banco de dados criado - Configuração de acesso para as salas concluída - Aula 04

--- src/database/init.js
+++ src/database/init.js
@@ -1,21 +1,23 @@
-const Database = require('./config')
+const Database = require('./config');
 
 const initDb = {
     async init(){
-        const db = await Database()
+        const db = await Database();
 
         await db.exec(`CREATE TABLE rooms (
-            id INT PRIMARY KEY,
-            pass VARCHAR(255)
+            id INTEGER PRIMARY KEY,
+            pass TEXT
         )`);
 
         await db.exec(`CREATE TABLE questions (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            title VARCHAR(255),
-            check INT
-        )`)
+            id INT PRIMARY KEY,
+            titulo TEXT,
+            read INTEGER
+        )`);
+
+        await db.close();
     }
 }
 
-initDb.init()
+initDb.init();
 

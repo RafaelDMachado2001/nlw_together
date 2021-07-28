f46bf4a73a1976b5a4128b695a4761217b09521c
Rafael Machado note <rafaeldmachado2001@gmail.com>
2021-07-28 13:00:06 -0300

    Corrigido problema no cadastro da senha das salas que possuiam letras

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -3,7 +3,7 @@ const Database = require("../database/config");
 
 // !-- 'module.exports' torna todo o trecho de código subsequente exportável e acessível nos demais arquivos do projeto --! //
 module.exports = {
-    // ?-- 'async' é o parâmetro necessário para tornar a função 'paciente'. Ele funciona em conjunto com o parâmetro 'awai' dentro do código, que tem a função de ordenar a execução do código. Ou seja, quando tiver o 'awai' antes do parâmetro, o Javascript irá executar completamente aquele trecho antes de passar para a próxima linha, evitando erros de inicialização já que estamos utilizando funções para enviar informações para o BD --? //
+    // ?-- 'async' é o parâmetro necessário para tornar a função 'paciente'. Ele funciona em conjunto com o parâmetro 'await' dentro do código, que tem a função de ordenar a execução do código. Ou seja, quando tiver o 'await' antes do parâmetro, o Javascript irá executar completamente aquele trecho antes de passar para a próxima linha, evitando erros de inicialização já que estamos utilizando funções para enviar informações para o BD --? //
     async create(req, res) {
         const db = await Database();
         // ? 'Pega' a senha de administrador cadastrada para criar a sala
@@ -28,7 +28,7 @@ module.exports = {
                     pass
                 ) VALUES (
                     ${parseInt(roomId)},
-                    ${pass}
+                    "${pass}"
                 )`);    
             }
         }
